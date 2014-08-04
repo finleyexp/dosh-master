4a4c9d86bdad5e0d18e95a8729d8a31a31a4e7d3
mere-mortal <dean@unwined.co.za>
2014-08-04 23:14:37 +0200

    dosh source

---
+++ src/p2p/p2p_networks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2014 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace nodetool
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0xA7 ,0x2A, 0xA1, 0xA6 , 0x6A, 0x1A , 0xA4, 0x1A, 0xA7, 0xA3, 0x3A, 0xA3, 0x7A, 0xA1, 0x1A, 0x6A} };
+}
